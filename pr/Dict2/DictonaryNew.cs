596a7261009e8bf33847a272f2aa67e1869f30d8
Azat <46015824+serikovskii@users.noreply.github.com>
2019-03-26 18:30:07 +0600

    практика

---
+++ pr/Dict2/DictonaryNew.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dict2
+{
+    public class DictonaryNew<T,U>
+    {
+        public T Key { get; set; }
+        public U Values { get; set; }
+    }
+}
---
+++ pr/Dict2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dict2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictonaryNew = new List<DictonaryNew<string, int>>();
+
+            string text = "Вот дом, Который построил Джек. А это пшеница, Которая в темном  чулане хранится В доме, " +
+                "Который построил Джек. А это веселая птица­ синица, Которая часто ворует пшеницу," +
+                " Которая в темном чулане хранится В доме, Который построил Джек.";
+            string[] separators = { ",", ".", "!", "?", ";", ":", " " };
+            string[] words = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            int count = 0;
+            foreach (string tx in words)
+            {
+                for (int i = 0; i < words.Length - 1; i++)
+                {
+                    if (tx == words[i])
+                    {
+                        count++;
+                    }
+                }
+                dictonaryNew.Add(new DictonaryNew<string, int> { Key = tx, Values = count});
+                //dictonaryNew.Values = count;
+
+                count = 0;
+            }
+            foreach (var dc in dictonaryNew)
+            {   
+                Console.WriteLine($"слово - {dc.Key} \t кол-во - {dc.Values}");
+            }
+           
+        }
+    }
+}
